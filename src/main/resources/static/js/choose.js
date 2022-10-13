919e00dd10f337a7f1e735f5c7a48b5f7ebc6e69
Eric <0524eric@gmail.com>
2022-10-13 11:11:59 +0800

    Finished

--- src/main/resources/static/js/choose.js
+++ src/main/resources/static/js/choose.js
@@ -7,34 +7,62 @@ $(document).ready(function() {
             url: "http://localhost:8080/SightAPI?zone="+$(this).text(),
             success: function(allSights){
               results.html("");
-              $.each(allSights,function (i,sight){
-                  let address = "https://www.google.com.tw/maps/place/"+sight.address;
-                  results.append(
-                        "<div class='card' id = o_card>" +
-                            "<div class='card-header'>"+sight.sightName+"</div>" +
-                            "<div class='card-body bg-dark text-white'>" +
-                                "<p>"+"區域:"+sight.zone+"<br>"+"分類:"+sight.category +
-                                    `<form action= ${address}  targe='_blank'> 
+              $.each(allSights,function (i,sight) {
+                  let address = "https://www.google.com.tw/maps/place/" + sight.address;
+                  if (sight.photoURL == "null") {
+                      results.append(
+                          "<div class='card' id = o_card>" +
+                          "<div class='card-header'>" + sight.sightName + "</div>" +
+                          "<div class='card-body bg-dark text-white'>" +
+                          "<p>" + "區域:" + sight.zone + "<br>" + "分類:" + sight.category +
+                          `<form action= ${address}  target='_blank'> 
                                         <button type='submit'>地址</button>
                                      </form>` +
-                                "</p>"+
-                                `<div class="card">
+                          "</p>" +
+                          `<div class="card">
                                     <div class="card-header">
                                         <a class="btn" data-bs-toggle="collapse" href="#collapse${i}">
                                             詳細資訊
                                         </a>
                                     </div>
                                     <div id="collapse${i}" class="collapse" data-bs-parent="#accordion">
-                                        <div class="card-body">
+                                        <div class="card-body">                                        
+                                             <p style="color: black;">${sight.description}</p>
+                                        </div>
+                                    </div>
+                                </div>` +
+
+                          "</div>" +
+                          "</div>"
+                      )
+                  } else{
+                      results.append(
+                          "<div class='card' id = o_card>" +
+                          "<div class='card-header'>" + sight.sightName + "</div>" +
+                          "<div class='card-body bg-dark text-white'>" +
+                          "<p>" + "區域:" + sight.zone + "<br>" + "分類:" + sight.category +
+                          `<form action= ${address}  target='_blank'> 
+                                        <button type='submit'>地址</button>
+                                     </form>` +
+                          "</p>" +
+                          `<div class="card">
+                                    <div class="card-header">
+                                        <a class="btn" data-bs-toggle="collapse" href="#collapse${i}">
+                                            詳細資訊
+                                        </a>
+                                    </div>
+                                    <div id="collapse${i}" class="collapse" data-bs-parent="#accordion">
+                                        <div class="card-body">                                            
                                             <img src=${sight.photoURL} alt="無圖片" />
                                              <p style="color: black;">${sight.description}</p>
                                         </div>
                                     </div>
-                                </div>`+
+                                </div>` +
 
-                        "</div>"+
-                  "</div>"
-                  )
+                          "</div>" +
+                          "</div>"
+                      )
+              }
               });
             }
         });
